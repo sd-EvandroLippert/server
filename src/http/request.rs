2142592c4a05c749904bdc361fc68c2d894d5b29
Evandro Lippert <evandro@selfdecode.com>
2021-10-05 16:35:53 -0300

    feat: Implemented TryFrom and ParseError

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,10 @@
 #![allow(unused)]
 
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Display, Result as FmtResult, Formatter, Debug};
+
 
 pub struct Request {
     
@@ -8,4 +12,55 @@ pub struct Request {
     path: String,
     query_string: Option<String>,  // Option<> Serve para dizer que esse parâmetro é opcional
     method: Method,
+}
+
+impl Request {
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
+        unimplemented!()
+    }
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+    
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error>{
+        unimplemented!()
+    }
+
+}
+
+pub enum ParseError{
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl Error for ParseError {
+
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult{
+        write!(f, "{}", self.message())
+    }
+
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult{
+        write!(f, "{}", self.message())
+    }
+
 }
